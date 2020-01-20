be565db6f47b40d3ea2813a45402c2b85ee8c13a
Mako Matsuno <mako_matsuno@amewatafuwari.local>
2020-01-20 10:39:06 +0900

    standardx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     
     "extends": [
-        "standard",
+        "standardx",
         "plugin:import/errors",
         "plugin:import/warnings",
         "plugin:import/typescript",
