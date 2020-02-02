c5d0f16a2be2baab543aebe761aeb1d822265fd1
Mako Matsuno <mako_matsuno@amewatafuwari.local>
2020-02-02 20:59:54 +0900

    image-file-error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const path = require('path')
 const withReactSvg = require('next-react-svg')
-
 module.exports = withReactSvg({
   include: path.resolve(__dirname, 'resources/image'),
+  webpack(config, options) {
+    return config
+  }
+})
+
+const withImages = require('next-images')
+module.exports = withImages({
   webpack(config, options) {
     return config
   }
