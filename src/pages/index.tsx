c759979cb85a18a01e13a003480cffd28c49090a
Mako Matsuno <mako_matsuno@amewatafuwari.local>
2020-03-01 04:03:28 +0900

    api呼べた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,42 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import CategoryText from '../components/atoms/CategoryText'
-import { NextPage } from 'next'
 import fetch from 'isomorphic-unfetch'
 
-const IndexPage: NextPage = () => {
-    return (
-        <Layout title="Home | Next.js + TypeScript Example">
-            <CategoryText>Foo</CategoryText>
-        </Layout>
-    )
+type Blog = {
+    contents: BlogContents[]
+    totalCount: number
+    offset: number
+    limit: number
 }
 
-fetch('https://yumedeshinou.microcms.io/api/v1/blog', {
-    headers: {
-        'X-API-KEY': process.env.BLOG_API_KEY as string,
-    },
-})
-    .then(res => res.json())
-    .then(res => console.log(res))
+type BlogContents = {
+    id: string
+    createdAt: string
+    updatedAt: string
+    blog_tittle: string
+    blog_text: string
+    blog_image: object
+}
+
+const IndexPage = ({ contents }: { contents: string }) => (
+    <>
+        <Layout title="Home | Next.js + TypeScript Example"></Layout>
+        <div> {contents} </div>
+    </>
+)
+
+IndexPage.getInitialProps = async () => {
+    const response = await fetch('https://yumedeshinou.microcms.io/api/v1/blog', {
+            headers: {
+                'X-API-KEY': process.env.BLOG_API_KEY as string,
+            },
+        }
+    )
+    const data = (await response.json()) as Blog
+    const contents = data.contents.map(contents => contents.createdAt)
+    console.log(data)
+    console.log(contents)
+    return { contents }
+}
 
 export default IndexPage
