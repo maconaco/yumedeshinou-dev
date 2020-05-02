9cf03d41b21f76918599a8bab68b403fe87e6027
maconaco <makomakopg@gmail.com>
2020-05-02 17:58:38 +0900

    Fix all prettier ploblem

--- src/components/molecules/MenuNav.tsx
+++ src/components/molecules/MenuNav.tsx
@@ -8,10 +8,12 @@ import BaseNavIcon, { IconType } from '../atoms/CategoryIcon'
 type NavProps = {
     children: JSX.Element | JSX.Element[]
 }
+
 type ItemProps = React.ComponentProps<typeof Li> & {
     href: LinkProps['href']
     icon: IconType
 }
+
 type MenuNav = React.FunctionComponent<NavProps> & {
     Item: typeof Item
 }
@@ -19,44 +21,52 @@ type MenuNav = React.FunctionComponent<NavProps> & {
 const Nav = styled.nav`
     background-color: #b5dfe8;
 `
+
 const Ul = styled.ul`
     display: flex;
     justify-content: center;
     margin: auto;
 
-    ${media.lessThan("medium")`
+    ${media.lessThan('medium')`
         justify-content: space-between;
         width: 81.25vw;
     `};
 
-    ${media.greaterThan("medium")`
+    ${media.greaterThan('medium')`
         width: 68.5vw;
     `};
 `
+
 const Li = styled.li`
     flex-shrink: 0;
     list-style: none;
     padding: 12px 24px;
 
-    ${media.lessThan("medium")`
+    ${media.lessThan('medium')`
         padding: .4em .8em;
     `};
 `
-const CategoryText = styled(BaseCategoryText)`
 
-    ${media.lessThan("medium")`
+const CategoryText = styled(BaseCategoryText)`
+    ${media.lessThan('medium')`
         display: none;
     `};
 `
+
 const CategoryIcon = styled(BaseNavIcon)`
     display: none;
 
-    ${media.lessThan("medium")`
+    ${media.lessThan('medium')`
         display: block;
     `};
 `
 
-const Item: React.FunctionComponent<ItemProps> = ({ href, icon, children, ...props }) => (
+const Item: React.FunctionComponent<ItemProps> = ({
+    href,
+    icon,
+    children,
+    ...props
+}) => (
     <Link href={href} passHref>
         <Li {...props}>
             <CategoryText>{children}</CategoryText>
@@ -64,11 +74,13 @@ const Item: React.FunctionComponent<ItemProps> = ({ href, icon, children, ...pro
         </Li>
     </Link>
 )
+
 const MenuNav: MenuNav = ({ children }) => (
     <Nav>
         <Ul>{children}</Ul>
     </Nav>
 )
+
 MenuNav.Item = Item
 
 export default MenuNav
